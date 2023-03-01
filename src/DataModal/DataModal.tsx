75242fdda3ff9d32d9c675279c11bdf6e05a653c
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-03-01 16:34:34 +0330

    create data anad data type modal project done

---
+++ src/DataModal/DataModal.tsx
@@ -0,0 +1,8 @@
+import { dataModelType } from '@/Model/DataModelType';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+export const dataMobileMenuItem:dataModelType[] =[
+   {id:1,title:'دو کارت را انتخاب کنید. اگر کارت‌ها یکسان باشند، ثبت می‌شوند در غیر این صورت مجددا کارت‌ها برمی‌گردند و شما می‌توانید دوباره تلاش کنید.',icon:<StarBorderIcon style={{width:'15px',height:'15px',color:'red'}}/>},
+   {id:2,title:'سعی کنید عکس پشت هر کارت و مکان آن کارت را به حافظه بسپارید.',icon:<StarBorderIcon style={{width:'15px',height:'15px',color:'red'}}/>},
+   {id:3,title:'زمانی که همه‌ی جفت کارت‌ها پیدا شوند شما برنده می‌شوید.',icon:<StarBorderIcon style={{width:'15px',height:'15px',color:'red'}}/>},
+   {id:4,title:'سعی کنید تا جای ممکن با تعداد کلیک کمتری برنده شوید تا امتیاز بیشتری کسب کنید.',icon:<StarBorderIcon style={{width:'15px',height:'15px',color:'red'}}/>},
+]
---
+++ src/Model/DataModelType.ts
@@ -0,0 +1,5 @@
+export interface dataModelType {
+    id:number,
+    icon:string|any,
+    title:string
+}
