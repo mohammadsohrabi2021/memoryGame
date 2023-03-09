f58e9c4453650465ae9dde433f7e8556293cb3a7
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-03-09 17:20:09 +0330

    refactor timerProgressBar done

--- src/components/TimerProgress/TimerProgress.tsx
+++ src/components/TimerProgress/TimerProgress.tsx
@@ -1,22 +1,37 @@
 import React from 'react'
-import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
+import CircularProgress, {
+    CircularProgressProps,
+  } from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 type Props = {}
 
-const TimerProgress = (props: LinearProgressProps & { value: number }) => {
+const TimerProgress = ( props: CircularProgressProps & { value: number },) => {
     return (
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Box sx={{ width: '100%', mr: 1 }}>
-                <LinearProgress  variant="determinate" {...props} />
-            </Box>
-            <Box sx={{ minWidth: 80 }}>
-                <Typography variant="body2" color="text.secondary">{`${Math.round(
-                    props.value,
-                )} زمان`}</Typography>
-            </Box>
+        <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+        <CircularProgress variant="determinate" {...props} />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography
+            variant="caption"
+            component="div"
+            color="text.secondary"
+            padding={'100px'}
+            fontWeight={'bold'}
+          >{`${Math.round(props.value)} زمان`}</Typography>
         </Box>
+      </Box>
     )
 }
 export default TimerProgress
