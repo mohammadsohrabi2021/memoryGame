6eaf52b86c62247d8c6c7c6aadfe7fada055f1b3
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-03-09 17:18:59 +0330

    refactor finalMessage done

--- src/components/FinalMessage/FinalMessage.tsx
+++ src/components/FinalMessage/FinalMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
@@ -10,9 +10,10 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    // width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    // border: '2px solid #000',
+    borderRadius: '10px',
     boxShadow: 24,
     p: 4,
 };
@@ -28,18 +29,18 @@ type Props = {
 const FinalMessage = ({ openFinalMessage, handleCloseFinalMessage, time, score, clickCounts }: Props) => {
     const [timeLeft, setTimeLeft] = useState(5);
     const router = useRouter()
-  
+
     useEffect(() => {
-      const timer = setTimeout(() => {
-        setTimeLeft((prevTime) => prevTime - 1);
-      }, 1000);
-  
-      if (timeLeft === 0) {
-        clearTimeout(timer);
-        router.push('/FinalGame')
-      }
-  
-      return () => clearTimeout(timer);
+        const timer = setTimeout(() => {
+            setTimeLeft((prevTime) => prevTime - 1);
+        }, 1000);
+
+        if (timeLeft === 0) {
+            clearTimeout(timer);
+            router.push('/FinalGame')
+        }
+
+        return () => clearTimeout(timer);
     }, [timeLeft, router]);
 
     return (
@@ -52,21 +53,28 @@ const FinalMessage = ({ openFinalMessage, handleCloseFinalMessage, time, score, 
                 aria-labelledby="keep-mounted-modal-title"
                 aria-describedby="keep-mounted-modal-description"
             >
-                <Box sx={style}>
-                    <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
+                <Box sx={style} width={{xs:'75%',sm:'500px'}} textAlign={'center'}>
+                    <Grid display={'flex'} justifyContent={'center'} alignItems={'center'} gap={2} >
+                        <Typography fontSize={{xs:'18px',sm:'28px'}} fontWeight={'bold'} color={score === 8 ?'blue':'gray'}>
+                            {
+                                score === 8 ? "برنده شدی!" : ' متاسفانه امروز برنده نشدید'
+                            }
+                        </Typography>
+                        {score === 8 ? <CloudIcon style={{ width: '50px', height: '50px', color: 'blue' }} /> : <CloudIcon style={{ width: '50px', height: '50px', color: 'gray' }} />}
+                    </Grid>
+                    <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }} my={3} fontWeight={'bold'}fontSize={'22px'}color={score === 8 ?'green':'red'}>
                         {
-                            score === 8 ? "برنده شدی!" : 'باختی'
+                            score === 8 ? <> {`تبریک میگم! توی ${clickCounts} کلیک برنده شدی :)`}</> : <> {`(:شما با ${clickCounts}  کلیک بازنده شدید `}</>
                         }
-                        <CloudIcon style={{ width: '50px', height: '50px' }} />
-                    </Typography>
-                    <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
-                        {
-                            score === 8 ? <> {`تبریک میگم! توی ${clickCounts} کلیک برنده شدی :)`}</> : <> {`تو با ${clickCounts}  کلیک بازنده شدی :)`}</>
-                        }
-                        {timeLeft}
+
 
                     </Typography>
-                   
+                    <Typography borderRadius={'3px'} bgcolor={score === 8 ? 'green':'red'} py={2}color={'#fff'} fontWeight={'bold'} fontSize={'18px'}>
+                        {
+                            score === 8 ? <>  {`بریم جایزتو بدیم ${timeLeft}`}</> : <>  {`بریم به صفحه اصلی سورچی ${timeLeft}`}</>
+                        }
+                    </Typography>
+
                 </Box>
             </Modal>
         </Grid>
